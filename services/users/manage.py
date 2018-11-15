c3fa2a1262c52801a585ae2873d50ec698abc1ba
mflynn-lanl <mflynn@lanl.gov>
2018-11-15 07:44:29 -0700

    initial commit

--- services/users/manage.py
+++ services/users/manage.py
@@ -6,7 +6,43 @@ import unittest
 from project import app, db
 
 
-cli = FlaskGroup(app)
+from project import create_app, db   # new
+from project.api.models import User  # new
+import coverage
+COV = coverage.coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+COV.start()
+app = create_app()  # new
+cli = FlaskGroup(create_app=create_app)  # new
+
+@cli.command()
+def cov():
+    """Runs the unit tests with coverage."""
+    tests = unittest.TestLoader().discover('project/tests')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        COV.html_report()
+        COV.erase()
+        return 0
+    return 1
+
+@cli.command()
+def seed_db():
+    """Seeds the database."""
+    db.session.add(User(username='mark', email="mcflynn617@gmail.com"))
+    db.session.add(User(username='mflynn', email="mflynn@lanl.gov"))
+    db.session.commit()
+
 
 # new
 @cli.command()
@@ -14,6 +50,7 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
+
 
 @cli.command()
 def test():
@@ -23,5 +60,7 @@ def test():
     if result.wasSuccessful():
         return 0
     return 1
+
+
 if __name__ == '__main__':
     cli()
